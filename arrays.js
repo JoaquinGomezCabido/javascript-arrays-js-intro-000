885d61181fc9d71b2773a2ecb5cdff73b82ed7f6
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-05 19:11:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,37 +1,37 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var addElementToBeginningOfArray = (a[], b) => {
+var addElementToBeginningOfArray = (a, b) => {
   return newArray = [b, ...a]
 }
 
-var destructivelyAddElementToBeginningOfArray = (a[],b) => {
+var destructivelyAddElementToBeginningOfArray = (a,b) => {
   return a = [b, ...a]
 }
 
-var addElementToEndOfArray = (a[], b) => {
+var addElementToEndOfArray = (a, b) => {
   return newArray = [...a, b]
 }
 
-var destructivelyAddElementToEndOfArray = (a[],b) => {
+var destructivelyAddElementToEndOfArray = (a,b) => {
   return a = [...a, b]
 }
 
-var accessElementInArray = (a[], i) => {
+var accessElementInArray = (a, i) => {
   return a[i]
 }
 
-var destructivelyRemoveElementFromBeginningOfArray = (a[]) => {
+var destructivelyRemoveElementFromBeginningOfArray = (a) => {
   return a.shift()
 }
 
-var removeElementFromBeginningOfArray = (a[]) => {
+var removeElementFromBeginningOfArray = (a) => {
   return a.slice(1)
 }
 
-var destructivelyRemoveElementFromEndOfArray = (a[]) => {
+var destructivelyRemoveElementFromEndOfArray = (a) => {
   return a.pop()
 }
 
-var removeElementFromEndOfArray = (a[]) => {
+var removeElementFromEndOfArray = (a) => {
   return a.slice(0, a.length -1)
 }
